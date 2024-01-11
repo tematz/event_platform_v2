429b65b85681ec45466a3fc09c307745a920157f
Matheus Vieira <matvno@gmail.com>
2024-01-11 13:19:22 -0300

    Add sign-in and sign-up pages, update dependencies, and create layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
 import './globals.css'
 
 const poppins = Poppins({ 
@@ -22,8 +24,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={poppins.variable}>{children}</body>
     </html>
+    </ClerkProvider>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,9 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
@@ -9,8 +13,22 @@ const Header = () => {
           <Image src='/assets/images/logo.svg' width={128} height={38} alt='Evently logo' />
         </Link>
 
-        <div className='flex w-32 justify-end gap-3'>
+        <SignedIn>
+          <nav className='md:flex-between hidden w-full max-w-xs'>
+            <NavItems />
+          </nav>
+        </SignedIn>
 
+        <div className='flex w-32 justify-end gap-3'>
+          <SignedIn>
+            <UserButton afterSignOutUrl='/' />
+            <MobileNav />
+          </SignedIn>
+        <SignedOut>
+          <Button asChild className='rounded-full' size='lg'>
+            <Link href='/sign-in'>Login</Link>
+          </Button>
+        </SignedOut>
         </div>
       </div>
     </header>
